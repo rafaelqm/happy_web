461a8ff67e49dd1991577f7e5cca791ee0a5c5ca
Rafael Querino Moreira <rafaelqm@gmail.com>
2020-10-19 09:28:09 -0300

    Finished layout and basic screens

---
+++ README.md
@@ -0,0 +1,14 @@
+# Project Happy - Take happyness to the world
+
+## Simple project to practice Typescript and ReactJS
+
+This is a web client to get data from a NodeJS API and show all the orphanages 
+in a location with info about visit schedule, and address.
+
+## Docker ready
+To avoid the need to install Node in version 12.18.4, it comes a docker-compose.yml file,
+so just run the docker compose and inside bash run the **npm start**.
+
+```
+docker-compose run --rm --service-ports dev
+```
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import "../styles/pages/landing.css";
 import logoImg from "../images/logo.svg";
 import { FiArrowRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 function Landing() {
   return (
@@ -19,9 +20,9 @@ function Landing() {
           <span>Franca</span>
         </div>
 
-        <a href="" className="enter-app">
+        <Link to="/app" className="enter-app">
           <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-        </a>
+        </Link>
       </div>
     </div>
   );
